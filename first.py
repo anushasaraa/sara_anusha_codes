c87efc3eaf0c3d7c7e79a52af7a17a0fa3f238ed
anushasaraa <anushasara224@gmai.com>
2026-02-02 18:50:40 +0530

    first code

---
+++ first.py
@@ -0,0 +1,9 @@
+print ("Hello")
+
+# git status  ---- It will check the status of the file changes
+
+# git add filename -------it will add the file to the staging repo
+
+# git commit -m "commit message"  -- it will identify the code change/versioning 
+
+# git push    --- it will  push the code to the repo
