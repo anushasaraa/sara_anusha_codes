928a62d89aa05cc7a161228099469cc5cbe3ebed
anushasaraa <anushasara224@gmail.com>
2026-03-25 08:35:46 +0530

    Create split_strings_by_separator.py

---
+++ split_strings_by_separator.py
@@ -0,0 +1,13 @@
+from typing import List
+
+class Solution:
+    def splitWordsBySeparator(self, words: List[str], separator: str) -> List[str]:
+        result = []
+                    
+        for word in words:
+            parts = word.split(separator)
+            for part in parts:
+                if part != "":
+                    result.append(part)
+                                                                                                            
+        return result
